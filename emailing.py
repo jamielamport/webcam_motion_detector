6e40e6e1fb35917afa7d39255d177bcfb206652d
Jamie Lamport <jamie.lamport@gmail.com>
2023-10-02 17:46:01 +0100

    App completed: Detects motion based on the differences detected in colour blocks between frames then alerts me via email. First python threaded application

--- emailing.py
+++ emailing.py
@@ -1,2 +1,28 @@
-def sendEmail():
-    print('blah')
+import smtplib
+import imghdr
+from email.message import EmailMessage
+
+email_password = 'ujor hcid qics eenv'
+email_user = "jamie.lamport@gmail.com"
+email_recipient = "jamie.lamport@gmail.com"
+
+def sendEmail(emailImage):
+    print("Email send function started")
+    email_message = EmailMessage()
+    email_message["Subject"] = "A new anomoly has been detected by your webcam"
+    email_message.set_content("Something has been detected by your webcam, take a look")
+
+    with open(emailImage, "rb") as file:
+        content = file.read()
+    email_message.add_attachment(content, maintype="image", subtype=imghdr.what(None, content))
+
+    gmail = smtplib.SMTP("smtp.gmail.com", 587)
+    gmail.ehlo()
+    gmail.starttls()
+    gmail.login(email_user, email_password)
+    gmail.sendmail(email_user, email_recipient, email_message.as_string())
+    gmail.quit()
+    print("Email send function ended")
+
+if __name__ == "__main__":
+    sendEmail(emailImage="images/1.png")
--- main.py
+++ main.py
@@ -1,29 +1,37 @@
 import cv2
 import time
+import glob
+import os
 from emailing import sendEmail
+from threading import Thread
 
 video = cv2.VideoCapture(0)
 time.sleep(1)
 
 first_frame = None
 status_list = [0, 0]
+count = 1
+
+def clean_folder():
+    print("Clean folder function started")
+    images = glob.glob("images/*.png")
+    for image in images:
+        os.remove(image)
+    print("Clean folder function ended")
 
 while True:
     status = 0
     check, frame = video.read()
     gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     gray_frame_gau = cv2.GaussianBlur(gray_frame, (21, 21), 0)
-    #cv2.imshow("My Video", gray_frame_gau)
 
     if first_frame is None:
         first_frame = gray_frame_gau
 
     delta_frame = cv2.absdiff(first_frame, gray_frame_gau)
-    #cv2.imshow("my Video", delta_frame)
 
     thresh_frame = cv2.threshold(delta_frame, 60, 255, cv2.THRESH_BINARY_INV)[1]
     dil_frame = cv2.dilate(thresh_frame, None, iterations=2)
-    #cv2.imshow("my Video", thresh_frame)
 
     contours, check = cv2.findContours(dil_frame, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
@@ -35,16 +43,25 @@ while True:
         rectangle = cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0))
         if rectangle.any():
             status = 1
+            cv2.imwrite(f"images/{count}.png", frame)
+            count = count + 1
+            all_images = glob.glob("images/*.png")
+            im_num = int(len(all_images) / 2)
+            captured_image = all_images[im_num]
 
     status_list.append(status)
     status_list = status_list[-2:]
 
     if status_list[0] == 1 and status_list[1] == 0:
         # Object has disappeared
-        sendEmail()
+        email_thread = Thread(target=sendEmail, args=(captured_image, ))
+        email_thread.daemon = True
+        clean_thread = Thread(target=clean_folder)
+        clean_thread.daemon = True
+
+        email_thread.start()
 
     print(status_list)
-
 
     cv2.imshow("Video", frame)
     key = cv2.waitKey(1)
@@ -53,6 +70,8 @@ while True:
         break
 
 video.release()
+
+clean_thread.start()
 
 
 
